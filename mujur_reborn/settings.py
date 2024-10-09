71b9ec278fa7a4ba5fe123d38eaa382fe76f63c7
Mabacore101 <valentinozhuo2@gmail.com>
2024-10-09 08:31:49 +0700

    Menambahkan fungsionalitas login, register, dan logout.

--- mujur_reborn/settings.py
+++ mujur_reborn/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mujur_reborn.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
