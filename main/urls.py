eae24ac9eadf15e7d635ec9e3e464445e3164fd1
Mabacore101 <valentinozhuo2@gmail.com>
2024-10-21 20:01:08 +0700

    Menambahkan form, return json, dan return html.

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from main.views import show_main, register, login_user, logout_user
+from main.views import show_main, register, login_user, logout_user, show_json
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('register/', register, name='register'),
+    path('json/', show_json, name='show_json'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
 ]
