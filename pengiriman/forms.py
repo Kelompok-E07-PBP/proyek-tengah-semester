8ee72c71f5eccb16a308981c5c92964ffedb33bc
Raffi Dary <rraffidh@gmail.com>
2024-10-22 13:46:05 +0700

    Changed models to include dropdown for cities in jakarta, changed postal code to integer field, and changed labels for form

--- pengiriman/forms.py
+++ pengiriman/forms.py
@@ -5,3 +5,9 @@ class PengirimanForm(forms.ModelForm):
     class Meta:
         model = Pengiriman
         fields = ['address', 'city', 'postal_code', 'courier']
+        labels = {
+            'address': 'Alamat',
+            'city': 'Kota',
+            'postal_code': 'Kode Pos',
+            'courier': 'Kurir',
+        }
