ef9b3a0f8799f1a21e753e74aea168c9462387c1
Mabacore101 <valentinozhuo2@gmail.com>
2024-10-24 20:29:05 +0700

    Menambahkan tombol AJAX di module edit.

--- edit/forms.py
+++ edit/forms.py
@@ -1,7 +1,16 @@
 from django.forms import ModelForm
 from main.models import Product
+from django.utils.html import strip_tags
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ["nama_produk", "kategori", "harga", "gambar_produk"]
+    
+    def clean_nama_produk(self):
+        nama_produk = self.cleaned_data["nama_produk"]
+        return strip_tags(nama_produk)
+
+    def clean_kategori(self):
+        kategori = self.cleaned_data["kategori"]
+        return strip_tags(kategori)
--- edit/urls.py
+++ edit/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from edit.views import show_edit_main, create_product_entry, show_json, forbidden_view, edit_product_entry
-from edit.views import delete_product_entry
+from edit.views import delete_product_entry, add_product_entry_ajax
 
 app_name = 'edit'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('forbidden/', forbidden_view, name='forbidden_view'),
     path('edit-product-entry/<uuid:id>', edit_product_entry, name='edit_product_entry'),
     path('delete/<uuid:id>', delete_product_entry, name='delete_product_entry'),
+    path('create-product-entry-ajax', add_product_entry_ajax, name='add_product_entry_ajax'),
 ]
