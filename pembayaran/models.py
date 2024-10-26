abcb2e02fa068c0f8817b208b76365958128e8c5
dikbayy <dicky.bayusadewo@gmail.com>
2024-10-26 13:05:53 +0700

    Memperbaiki routing serta halaman pembayaran

--- pembayaran/models.py
+++ pembayaran/models.py
@@ -1,30 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class PaymentMethod(models.Model):
-    METHOD_CHOICES = [
-        ('Kartu Kredit', 'Kartu Kredit'),
-        ('Kartu Debit', 'Kartu Debit'),
-        ('Transfer Bank', 'Transfer Bank'),
-        ('E-Wallet', 'E-Wallet'),
-
-    ] 
-
-    name = models.CharField(max_length=100, choices=METHOD_CHOICES)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
 class Pembayaran(models.Model):
     STATUS_CHOICES = [
         ('PENDING', 'Pending'),
         ('SUCCESS', 'Success'),
         ('FAILED', 'Failed'),
     ]
+
+    
+    METHOD_CHOICES = [
+        ('Kartu Kredit', 'Kartu Kredit'),
+        ('Kartu Debit', 'Kartu Debit'),
+        ('Transfer Bank', 'Transfer Bank'),
+        ('E-Wallet', 'E-Wallet'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    payment_method = models.ForeignKey(PaymentMethod, on_delete=models.SET_NULL, null=True, blank=True)
+    payment_method = models.CharField(max_length=100, choices=METHOD_CHOICES)  
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- pengiriman/urls.py
+++ pengiriman/urls.py
@@ -4,6 +4,6 @@ from pengiriman.views import pengiriman_view, show_json
 app_name = 'pengiriman'
 
 urlpatterns = [
-    path('<uuid:product_id>/', pengiriman_view, name='pengiriman'),  
+    path('pengiriman/', pengiriman_view, name='pengiriman'), 
     path('json/', show_json, name='show_json_pengiriman'),
 ]
