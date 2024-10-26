41c335b895a600eea77183418f936c8f8746a265
Mabacore101 <valentinozhuo2@gmail.com>
2024-10-26 08:38:11 +0700

    Menambahkan filter, styling halaman tertentu, dan sedikit unit testing.

--- edit/tests.py
+++ edit/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.utils import timezone
+from main.models import Product
 
-# Create your tests here.
+class editTest(TestCase):
+    def test_edit_main_redirects_to_login(self):
+        response = self.client.get(reverse('edit:show_edit_main'))
+        self.assertEqual(response.status_code, 302)
+        redirect_url = response['Location']
+        self.assertTrue(redirect_url.startswith('/login'))
+        self.assertIn('next=', redirect_url)
+
+
+    # def test_main_using_main_template(self):
+    #     response = Client().get('')
+    #     self.assertTemplateUsed(response, 'main.html')
+
+    # def test_nonexistent_page(self):
+    #     response = Client().get('/random/')
+    #     self.assertEqual(response.status_code, 404)
+
+    # def test_strong_mood_user(self):
+    #     now = timezone.now()
+    #     mood = MoodEntry.objects.create(
+    #       mood="LUMAYAN SENANG",
+    #       time = now,
+    #       feelings = "senang sih, cuman tadi baju aku basah kena hujan :(",
+    #       mood_intensity = 8,
+    #     )
+    #     self.assertTrue(mood.is_mood_strong)
