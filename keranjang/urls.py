c123301557251caf1650ecf9cc9c1e673d24f373
Krisna Putra Purnomo <krisna.putra@ui.ac.id>
2024-10-26 22:45:22 +0700

    Done Styling

--- keranjang/urls.py
+++ keranjang/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from keranjang.views import keranjang_detail, tambah_ke_keranjang, hapus_dari_keranjang, update_keranjang, checkout, tambah_ke_keranjang_ajax
+from keranjang.views import keranjang_detail, tambah_ke_keranjang, hapus_dari_keranjang, update_keranjang, checkout, tambah_ke_keranjang_ajax, update_keranjang_ajax, hapus_dari_keranjang_ajax
 
 urlpatterns = [
     path('keranjang/', keranjang_detail, name='keranjang_detail'),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('update/<int:item_id>/', update_keranjang, name='update_keranjang'),
     path('checkout/', checkout, name='checkout'),
     path('tambah-ke-keranjang-ajax/<str:product_id>/', tambah_ke_keranjang_ajax, name='tambah_ke_keranjang_ajax'),
+    path('update-keranjang-ajax/<int:item_id>/', update_keranjang_ajax, name='update_keranjang_ajax'),
+    path('hapus-dari-keranjang-ajax/<int:item_id>/', hapus_dari_keranjang_ajax, name='hapus_dari_keranjang_ajax')
 ]
