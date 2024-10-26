a8408b3b732f572f4feedf28c4eaefbaff8f303c
Mabacore101 <valentinozhuo2@gmail.com>
2024-10-26 21:03:14 +0700

    Melakukan styling pada halaman ulasan.

--- ulasan/urls.py
+++ ulasan/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from ulasan.views import show_ulasan_main, create_ulasan_entry, show_json
+from ulasan.views import show_ulasan_main, create_ulasan_entry, show_json, edit_ulasan, delete_ulasan
 
 app_name = 'ulasan'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', show_ulasan_main, name='show_ulasan_main'),
     path('create-ulasan-entry', create_ulasan_entry, name='create_ulasan_entry'),
     path('json/', show_json, name='show_json'),
+    path('edit-ulasan/<uuid:id>', edit_ulasan, name='edit_ulasan'),
+    path('delete/<uuid:id>', delete_ulasan, name='delete_ulasan'),
 ]
