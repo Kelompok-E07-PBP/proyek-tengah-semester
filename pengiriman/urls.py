c3f17181c08fa50c7a58829e0e730730bdaab95a
Raffi Dary <rraffidh@gmail.com>
2024-10-26 19:42:45 +0700

    Added ajax to pengiriman

--- pengiriman/urls.py
+++ pengiriman/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from pengiriman.views import pengiriman_view, show_json
+from pengiriman.views import pengiriman_view, show_json, process_pengiriman_ajax
 
 app_name = 'pengiriman'
 
 urlpatterns = [
     path('pengiriman/', pengiriman_view, name='pengiriman'), 
     path('json/', show_json, name='show_json_pengiriman'),
+    path('process_pengiriman_ajax/', process_pengiriman_ajax, name='process_pengiriman_ajax'),
 ]
--- pengiriman/views.py
+++ pengiriman/views.py
@@ -4,7 +4,7 @@ from pengiriman.models import Pengiriman
 from pengiriman.forms import PengirimanForm
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from keranjang.models import Keranjang
 from django.urls import reverse
 
@@ -25,7 +25,43 @@ def pengiriman_view(request):
     else:
         form = PengirimanForm()
 
-    return render(request, 'pengiriman.html', {'form': form, 'items': items, 'keranjang': keranjang})
+    return render(request, 'pengiriman.html', {
+        'form': form,
+        'items': items,
+        'keranjang': keranjang
+    })
+
+@login_required(login_url='/login')
+def process_pengiriman_ajax(request):
+    if request.method == "POST" and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        user = request.user
+        keranjang = get_object_or_404(Keranjang, user=user)
+
+        if not keranjang.itemkeranjang_set.exists():
+            return JsonResponse({'error': 'Keranjang kosong, tidak dapat melakukan pengiriman.'}, status=400)
+
+        form_data = request.POST
+        pengiriman = Pengiriman(
+            user=user,
+            first_name=form_data.get('first_name'),
+            last_name=form_data.get('last_name'),
+            email=form_data.get('email'),
+            phone_number=form_data.get('phone_number'),
+            address=form_data.get('address'),
+            city=form_data.get('city'),
+            postal_code=form_data.get('postal_code'),
+            courier=form_data.get('courier')
+        )
+        
+        try:
+            pengiriman.save()
+
+            return JsonResponse({'message': 'Pengiriman berhasil dibuat!', 'pengiriman_id': pengiriman.id})
+        except Exception as e:
+            print("Error during pengiriman processing:", e)
+            return JsonResponse({'error': 'Terjadi kesalahan saat melakukan pengiriman.'}, status=500)
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
 
 def show_json(request):
     data = Pengiriman.objects.all()
