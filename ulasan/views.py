942fd4e8760a6e1e1bc411016a4e0836330be965
dikbayy <dicky.bayusadewo@gmail.com>
2024-12-19 12:16:07 +0700

    Update Review page punya hamid

--- ulasan/views.py
+++ ulasan/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
+from django.shortcuts import render, redirect, get_object_or_404  # Tambahkan get_object_or_404
 from ulasan.forms import UlasanEntryForm
 from ulasan.models import UlasanEntry
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core import serializers
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
-from django.utils import timezone
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
@@ -20,12 +18,10 @@ def show_ulasan_main(request):
         'current_username': current_username,
         'nama_halaman' : 'Ulasan',
     }
-
     return render(request, "ulasan_main.html", context)
 
-def create_ulasan_entry(request):
 
-    # Bagian ini akan link komentar dengan seorang user.
+def create_ulasan_entry(request):
     if request.method == 'POST':
         form = UlasanEntryForm(request.POST)
         if form.is_valid():
@@ -39,46 +35,123 @@ def create_ulasan_entry(request):
     context = {'form': form}
     return render(request, 'create_ulasan_entry.html', context)
 
+
 @api_view(['GET'])
 def show_json(request):
     data = UlasanEntry.objects.all()
     serializer = UlasanEntrySerializer(data, many=True)
     return Response(serializer.data)
 
+
 @login_required(login_url='/login')
 def edit_ulasan(request, id):
-    # Ambil Ulasan berdasarkan id
-    ulasan = UlasanEntry.objects.get(pk = id)
-
-    # Set ulasan entry sebagai instance dari form
+    ulasan = UlasanEntry.objects.get(pk=id)
     form = UlasanEntryForm(request.POST or None, instance=ulasan)
 
     if form.is_valid() and request.method == "POST":
-        # Simpan form dan kembali ke halaman awal
         form.save()
         return HttpResponseRedirect(reverse('ulasan:show_ulasan_main'))
 
     context = {'form': form}
     return render(request, "edit_ulasan.html", context)
 
+
 def delete_ulasan(request, id):
-    ulasan = UlasanEntry.objects.get(pk = id)
+    ulasan = UlasanEntry.objects.get(pk=id)
     ulasan.delete()
     return HttpResponseRedirect(reverse('ulasan:show_ulasan_main'))
+
 
 @csrf_exempt
 @require_POST
 def add_ulasan_entry_ajax(request):
-    user = request.user
-    nama_produk_ulas = strip_tags(request.POST.get("nama_produk_ulas"))
-    rating = request.POST.get("rating")
-    komentar = strip_tags(request.POST.get("komentar"))
-    
+    try:
+        user = request.user
+        nama_produk_ulas = strip_tags(request.POST.get("nama_produk_ulas"))
+        rating = request.POST.get("rating")
+        komentar = strip_tags(request.POST.get("komentar"))
 
-    new_ulasan = UlasanEntry(
-        user=user, nama_produk_ulas=nama_produk_ulas,
-        rating=rating, komentar=komentar
-    )
-    new_ulasan.save()
+        new_ulasan = UlasanEntry(
+            user=user, 
+            nama_produk_ulas=nama_produk_ulas,
+            rating=rating, 
+            komentar=komentar
+        )
+        new_ulasan.save()
 
-    return HttpResponse(b"CREATED", status=201)
+        response_data = {
+            'success': True,
+            'message': 'Ulasan berhasil ditambahkan',
+            'data': {
+                'id': str(new_ulasan.id),
+                'nama_produk_ulas': new_ulasan.nama_produk_ulas,
+                'rating': new_ulasan.rating,
+                'komentar': new_ulasan.komentar,
+                'waktu': new_ulasan.waktu.strftime('%Y-%m-%d %H:%M:%S'),
+            }
+        }
+
+        return JsonResponse(response_data, status=201)
+
+    except Exception as e:
+        return JsonResponse({
+            'success': False,
+            'message': f'Terjadi kesalahan: {str(e)}'
+        }, status=500)
+
+
+@csrf_exempt
+@require_POST
+def edit_ulasan_ajax(request, id):
+    try:
+        ulasan = get_object_or_404(UlasanEntry, pk=id, user=request.user)
+        
+        nama_produk_ulas = strip_tags(request.POST.get("nama_produk_ulas"))
+        rating = request.POST.get("rating")
+        komentar = strip_tags(request.POST.get("komentar"))
+        
+        ulasan.nama_produk_ulas = nama_produk_ulas
+        ulasan.rating = rating
+        ulasan.komentar = komentar
+        ulasan.save()
+
+        response_data = {
+            'success': True,
+            'message': 'Ulasan berhasil diperbarui',
+            'data': {
+                'id': str(ulasan.id),
+                'nama_produk_ulas': ulasan.nama_produk_ulas,
+                'rating': ulasan.rating,
+                'komentar': ulasan.komentar,
+                'waktu': ulasan.waktu.strftime('%Y-%m-%d %H:%M:%S'),
+            }
+        }
+
+        return JsonResponse(response_data, status=200)
+
+    except Exception as e:
+        return JsonResponse({
+            'success': False,
+            'message': f'Terjadi kesalahan: {str(e)}'
+        }, status=500)
+
+
+@csrf_exempt
+@require_POST
+def delete_ulasan_ajax(request, id):
+    try:
+        ulasan = get_object_or_404(UlasanEntry, pk=id, user=request.user)
+        ulasan.delete()
+
+        response_data = {
+            'success': True,
+            'message': 'Ulasan berhasil dihapus'
+        }
+
+        return JsonResponse(response_data, status=200)
+
+    except Exception as e:
+        return JsonResponse({
+            'success': False,
+            'message': f'Terjadi kesalahan: {str(e)}'
+        }, status=500)
